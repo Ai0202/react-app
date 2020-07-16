71d43ccfeba13fadd98dbfe75cb784299af09926
Atsushi <atsushi88885@gmail.com>
2020-07-16 15:46:42 +0900

    微修正

--- src/components/pages/Members/Members.tsx
+++ src/components/pages/Members/Members.tsx
@@ -1,11 +1,8 @@
 import React from "react"
 import MemberList from "../../organisms/MemberList"
 
-
 export const Members: React.FC = () => {
   return (
-    <div>
-      <MemberList />
-    </div>
+    <MemberList />
   )
 }
