5ddf3fa4cfcf60124adacded3d42f371c8add517
Atsushi <atsushi88885@gmail.com>
2020-07-18 23:52:57 +0900

    編集削除ボタン追加

--- src/components/organisms/Member/Member.tsx
+++ src/components/organisms/Member/Member.tsx
@@ -1,13 +1,9 @@
 import React from "react"
 import { makeStyles } from "@material-ui/core/styles"
-import Card from "@material-ui/core/Card"
-import CardActionArea from "@material-ui/core/CardActionArea"
-import CardContent from "@material-ui/core/CardContent"
-import CardMedia from "@material-ui/core/CardMedia"
 import Typography from "@material-ui/core/Typography"
-
-import { CardTitle } from "../../atoms/CardTitle"
-import { CardDetail } from "../../atoms/CardDetail"
+import { Card, CardMedia, CardContent, CardActions, IconButton } from "@material-ui/core"
+import EditIcon from "@material-ui/icons/Edit"
+import DeleteIcon from "@material-ui/icons/Delete"
 
 const useStyles = makeStyles({
   root: {
@@ -16,6 +12,15 @@ const useStyles = makeStyles({
   media: {
     height: 200,
   },
+  title: {
+    fontSize: '2rem',
+    color: '#CD8724',
+    fontFamily: "'Rowo Typeface', 'Dancing Script', 'Sawarabi Gothic', 'san-serif'",
+    letterSpacing: '2px',
+  },
+  pullLeft: {
+    marginLeft: 'auto',
+  }
 })
 
 type Props = {
@@ -31,23 +36,28 @@ export const Member: React.FC<Props> = ({member}: Props) => {
   
   return (
     <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={member.image}
-          title="Atsushi Ikeda"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h4">
-            <CardTitle name={member.name} />
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            <CardDetail 
-              description={member.description}
-            />
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+      <CardMedia
+        className={classes.media}
+        image={member.image}
+        title="Atsushi Ikeda"
+      />
+      <CardContent>
+        <Typography className={classes.title}  gutterBottom variant="h5" component="h4">
+          member.name
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+          member.description
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <IconButton className={classes.pullLeft}>
+          <EditIcon />
+        </IconButton>
+        <IconButton className={classes.pullLeft}>
+          <DeleteIcon />
+        </IconButton>
+      </CardActions>
+
     </Card>
   )
 }
